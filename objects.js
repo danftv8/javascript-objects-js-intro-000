b9289b7d3b51873133fa0e8a8c2e4624949fa57c
Daniel Mullaney <danftv@gmail.com>
2021-01-12 04:18:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {song:"title"};
 
 function updatePlaylist(playlist, artistName, songTitle){
-   return Object.assign({}, playlist, {[artistName]: Phil OchsTitle })
+   return Object.assign({}, playlist, {[artistName]: songTitle })
 }
