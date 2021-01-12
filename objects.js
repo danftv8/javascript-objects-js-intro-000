d869a2385ab99228e718661d76e7815173083bf2
Daniel Mullaney <danftv@gmail.com>
2021-01-12 04:01:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
   artist: "song"
 };
-function updatePlaylist(playlist, artistName, songTitle) {
+var updatePlaylist = function(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
